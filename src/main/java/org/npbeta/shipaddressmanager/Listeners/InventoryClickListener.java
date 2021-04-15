69ac1711b7cdc58bbd7aab20dd16c580679288a3
BuildTools <admin@npbeta.com>
2021-04-15 21:07:40 +0800

    Added Listeners, Commands, Permissions & Configs

---
+++ src/main/java/org/npbeta/shipaddressmanager/Listeners/InventoryClickListener.java
@@ -0,0 +1,6 @@
+package org.npbeta.shipaddressmanager.Listeners;
+
+import org.bukkit.event.Listener;
+
+public class InventoryClickListener implements Listener {
+}
--- src/main/java/org/npbeta/shipaddressmanager/ShipAddressManager.java
+++ src/main/java/org/npbeta/shipaddressmanager/ShipAddressManager.java
@@ -1,17 +1,45 @@
 package org.npbeta.shipaddressmanager;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.npbeta.shipaddressmanager.Listeners.ChatListener;
+import org.npbeta.shipaddressmanager.Listeners.InventoryClickListener;
+
+import java.util.Objects;
 
 public final class ShipAddressManager extends JavaPlugin {
 
+    private String address;
+    private int port;
+    private String database;
+    private String username;
+    private String password;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.getLogger().info("Welcome to ShipAddressManager!");// Plugin startup logic
 
+        getLogger().info("Registering Listener...");
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
+        getLogger().info("Registering Commands...");
+        Objects.requireNonNull(getCommand("sam")).setExecutor(new CommandHandler());
+        getLogger().info("Reading Config...");
+        loadConfig();
+        getLogger().info("Initialize Complete.");
+        getLogger().info("Read from config.yml:" + address + port + database + username + password);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        this.getLogger().info("Thanks For Using ShipAddressManager!");// Plugin shutdown logic
+    }
+
+    void loadConfig() {
+        this.saveDefaultConfig();
+        this.address = this.getConfig().getString("mysql.address");
+        this.port = this.getConfig().getInt("mysql.port");
+        this.database = this.getConfig().getString("mysql.database");
+        this.username = this.getConfig().getString("mysql.username");
+        this.password = this.getConfig().getString("mysql.password");
     }
 }
